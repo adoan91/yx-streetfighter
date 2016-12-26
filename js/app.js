8bbbfbdce3eb6cad639562f04f64150e78c819d1
Anthony Doan <anthonydoan@csus.edu>
2016-12-25 16:21:56 -0800

    Added X key functionality
    Implemented X key press event and put instructions on the page.

--- js/app.js
+++ js/app.js
@@ -33,10 +33,36 @@ $(document).ready(function() {
 		$(".ryu-throwing").hide();
 		$(".ryu-ready").show();
 	});
+
+	$(document).keydown(function(e) {
+		if (e.keyCode == 88) {
+			//playCool();
+			$(".ryu-ready").hide();
+			$(".ryu-still").hide();
+			$(".ryu-cool").show();
+		}
+	}).keyup(function(e) {
+		if (e.keyCode == 88) {
+			//pauseCool();
+			$(".ryu-cool").hide();
+			$(".ryu-still").show();
+		}
+	});
 });
 
 function playHadouken() {
 	$("#hadouken-sound")[0].volume = 0.5;
 	$("#hadouken-sound")[0].load();
 	$("#hadouken-sound")[0].play();
+}
+
+function playCool() {
+	$("#cool-sound")[0].volume = 0.5;
+	$("#cool-sound")[0].load();
+	$("#cool-sound")[0].play();
+}
+
+function pauseCool() {
+	$("#cool-sound")[0].pause();
+	$("#cool-sound")[0].load();
 }
